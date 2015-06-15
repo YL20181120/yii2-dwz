eb09987e41b51b9e301dd32983e0993906f3f7bc
KillMeAgain <youjingqiang@vip.qq.com>
2015-06-15 17:01:06 +0800

    Signed-off-by: KillMeAgain <youjingqiang@vip.qq.com>

--- Nav.php
+++ Nav.php
@@ -10,7 +10,7 @@ use yii\helpers\Html;
 class Nav extends Widget
 {
 	public $items = [];
-	public $_items = '';
+	public $_items = [];
 
 	public $params;
 
@@ -26,68 +26,24 @@ class Nav extends Widget
 	}
 	public function run(){
 		DwzAsset::register($this->getView());
-		//$this->renderItems($this->items);
-		$this->_items = '<div class="accordionHeader">
-						<h2><span>Folder</span>界面组件</h2>
-					</div>
-					<div class="accordionContent">
-						<ul class="tree treeFolder">
-							<li><a href="tabsPage.html" target="navTab">主框架面板</a>
-								<ul>
-									<li><a href="main.html" target="navTab" rel="main">我的主页</a></li>
-								</ul>
-							</li>
-							
-							<li><a>常用组件</a>
-								<ul>
-									<li><a href="w_panel.html" target="navTab" rel="w_panel">面板</a></li>
-								</ul>
-							</li>
-							<li><a href="dwz.frag.xml" target="navTab" external="true">dwz.frag.xml</a></li>
-						</ul>
-					</div>
-					<div class="accordionHeader">
-						<h2><span>Folder</span>典型页面</h2>
-					</div>
-					<div class="accordionContent">
-						<ul class="tree treeFolder treeCheck">
-							<li><a href="demo_page1.html" target="navTab" rel="demo_page1">查询我的客户</a></li>
-							<li><a href="demo_page1.html" target="navTab" rel="demo_page2">表单查询页面</a></li>
-							<li><a href="demo_page4.html" target="navTab" rel="demo_page4">表单录入页面</a></li>
-							<li><a href="demo_page5.html" target="navTab" rel="demo_page5">有文本输入的表单</a></li>
-							<li><a href="javascript:;">有提示的表单输入页面</a>
-								<ul>
-									<li><a href="javascript:;">页面一</a></li>
-									<li><a href="javascript:;">页面二</a></li>
-								</ul>
-							</li>
-							<li><a href="javascript:;">选项卡和图形的页面</a>
-								<ul>
-									<li><a href="javascript:;">页面一</a></li>
-									<li><a href="javascript:;">页面二</a></li>
-								</ul>
-							</li>
-							<li><a href="javascript:;">选项卡和图形切换的页面</a></li>
-							<li><a href="javascript:;">左右两个互动的页面</a></li>
-							<li><a href="javascript:;">列表输入的页面</a></li>
-							<li><a href="javascript:;">双层栏目列表的页面</a></li>
-						</ul>
-					</div>';
-		return Html::tag('div',$this->_items,['class'=>'accordion','fillSpace'=>'sidebar']);
+		$this->renderItems($this->items);
+		//return Html::tag('div',$this->_items,['class'=>'accordion','fillSpace'=>'sidebar']);
+		var_dump($this->_items);
 	}
 
 	public function renderItems($_items){
         foreach ($_items as $i => $item) {
+        	//判断是否为叶子节点
             if (isset($item['visible']) && !$item['visible']) {
                 continue;
             } elseif(isset($item['menus']) && count($item['menus'])){
             	$_t = $item['menus'];
             	unset($item['menus']);
-            	$this->_items .= Html::tag('ul',$this->renderItem($item),['class' => 'tree treeFolder']);
+            	$this->_items[] = $this->renderItem($item);
             	$this->renderItems($_t);
             	continue;
             }
-            $this->_items .= $this->renderItem($item);
+            $this->_items[] = $this->renderItem($item);
         }
 	}
 
@@ -100,13 +56,19 @@ class Nav extends Widget
 		if(!isset($item['label'])){
 			throw new InvalidConfigException("The 'label' option is required.");
 		}
-
+		return $item;
+	}
+	//普通顶结点
+	protected function renderRoot($item){
+		return Html::tag('div',Html::tag('h2',Html::tag('span','Folder').$item['label']));
+	}
+	//叶子节点
+	protected function renderLeaf($item){
+		return Html::tag('li',Html::a($item['label'],$item['url'],['target'=>'navTab']));
 	}
 
-	protected function renderHeader($item){
-		return Html::tag('div',Html::tag('h2',Html::tag('span','Folder').$item['label']),['class'=>'accordionHeader']);
-	}
-	protected function renderContent($content){
+	//中间节点,应该还有子节点
+	protected function renderMiddle($item) {
 
 	}
 }
