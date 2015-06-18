8d9b5ad36c8f5c1bb8e716c38b2a69c9369ee1c5
KillMeAgain <youjingqiang@vip.qq.com>
2015-06-18 18:08:42 +0800

    GridView

--- grid/GridView.php
+++ grid/GridView.php
@@ -1,9 +1,31 @@
 <?php
 namespace yii\dwz\grid;
-/**
-* 
-*/
-class GridView extends Widget
+use yii\grid\GridView as Grid;
+use yii\grid\DataColumn;
+use yii\helpers\Html;
+class GridView extends Grid
 {
+	public $rel;
+	public $summary = false;
+	public $tableOptions = ['class' => 'table','style' => 'width:100%;'];
 
+	public function renderSummary(){
+		if(!$this->summary){
+
+		} else {
+			return null;
+		}
+	}
+	public function init(){
+		parent::init();
+		foreach ($this->columns as $i => $column) {
+			if($column instanceof DataColumn) {
+				$column->sortLinkOptions = ['target' => 'navTab','rel' => $this->rel,'class' => 'j-ajax'];
+				$this->columns[$i] = $column;
+			}
+		}
+	}
+	public function renderPager(){
+		return 'pager';
+	}
 }
