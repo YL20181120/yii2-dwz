836d226757f397e608bf79a3c941aeeb7bc42be5
KillMeAgain <youjingqiang@vip.qq.com>
2015-06-24 22:30:56 +0800

    Sort

--- grid/DataColumn.php
+++ grid/DataColumn.php
@@ -13,4 +13,12 @@ class DataColumn extends \yii\grid\Column
 	public $label;
 
 	public $enableSorting = false;
+
+	public $value;
+
+	public function renderHeaderCellContent(){
+        $provider = $this->grid->dataProvider;
+		//\yii\helpers\VarDumper::dump($provider);
+		return "hh";
+	}
 }
