a229a68d5048873a7482d46866409d5ab6536dc9
KillMeAgain <youjingqiang@vip.qq.com>
2015-07-10 14:48:22 +0800

    file uploader

--- helpers/Html.php
+++ helpers/Html.php
@@ -12,6 +12,35 @@ class Html extends \yii\helpers\BaseHtml
         $for = array_key_exists('for', $options) ? $options['for'] : static::getInputId($model, $attribute);
         $attribute = static::getAttributeName($attribute);
         $label = isset($options['label']) ? $options['label'] : static::encode($model->getAttributeLabel($attribute));
-        return $label;
-    }   
+        return static::tag('dt',$label);
+    } 
+    public static function error($model, $attribute, $options = [])
+    {
+        $attribute = static::getAttributeName($attribute);
+        $error = $model->getFirstError($attribute);
+        if($error != null) {
+	        $tag = isset($options['tag']) ? $options['tag'] : 'div';
+	        $encode = !isset($options['encode']) || $options['encode'] !== false;
+	        unset($options['tag'], $options['encode']);
+	        return Html::tag($tag, $encode ? Html::encode($error) : $error, $options);
+	    } else {
+	    	return "";
+	    }
+    }
+
+/*    public static function activeTextInput($model, $attribute, $options = []) {
+    	$inputID = Html::getInputId($model, $attribute);
+        $attribute = Html::getAttributeName($attribute);
+        $options = $options;
+        $class = isset($options['class']) ? [$options['class']] : [];
+        $class[] = "field-$inputID";
+        if ($model->isAttributeRequired($attribute)) {
+            $class[] = $form->requiredCssClass;
+        }
+        if ($model->hasErrors($attribute)) {
+            $class[] = $form->errorCssClass;
+        }
+        $class = implode(' ', $class);
+        var_dump($class);
+    }*/
 }
--- widgets/ActiveForm.php
+++ widgets/ActiveForm.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace yii\dwz\widgets;
-
+use yii\dwz\helpers\Html;
+use yii\base\InvalidCallException;
+use yii\helpers\Json;
 /**
 * 使用table布局
 */
@@ -10,5 +12,40 @@ class ActiveForm extends \yii\widgets\ActiveForm
 
 	public $fieldClass = 'yii\dwz\widgets\ActiveField';
 
-	public $options = ['class' => 'pageFormContent'];
+	public $options = ['class' => 'pageForm required-validate','onsubmit'=>'return validateCallback(this)','novalidate'=>'novalidate'];
+
+	public $formDivOptions = ['class' => 'pageFormContent nowrap','layoutH' => '100'];
+
+    public $errorCssClass = 'error';
+
+
+    /**
+     * Initializes the widget.
+     * This renders the form open tag.
+     */
+    public function init()
+    {
+        if (!isset($this->options['id'])) {
+            $this->options['id'] = $this->getId();
+        }
+        echo Html::beginForm($this->action, $this->method, $this->options);
+        echo Html::beginTag('div',$this->formDivOptions);
+    }
+
+    /**
+     * Runs the widget.
+     * This registers the necessary javascript code and renders the form close tag.
+     * @throws InvalidCallException if `beginField()` and `endField()` calls are not matching
+     */
+    public function run()
+    {
+        if (!empty($this->_fields)) {
+            throw new InvalidCallException('Each beginField() should have a matching endField() call.');
+        }
+
+        if ($this->enableClientScript) {
+        }
+        echo Html::endTag('div');
+        echo Html::endForm();
+    }	
 }
